fb2d8abc1abeb32ea9377098603f9fa63f0f8689
DevHobbyist <l3alazskiss15@gmail.com>
2022-10-05 14:15:11 -0300

    Added code

--- Methods 2/Program.cs
+++ Methods 2/Program.cs
@@ -8,8 +8,35 @@ namespace Methods_2
 {
     internal class Program
     {
+        // declarations
+        static int score;
+        static int health;
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods:");
+            Console.WriteLine();
+
+            // initializations
+            score = 0;
+            health = 100;
+
+            ShowHUD(); // usage
+
+            // earn points
+            score = score + 500;
+            // get hurt
+            health = health - 10;
+
+            ShowHUD(); // usage
+
+            Console.ReadKey(true);
+        }
+        static void ShowHUD() // declaration
+        {
+            // display HUD
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine();
         }
     }
 }
